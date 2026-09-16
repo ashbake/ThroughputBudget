00e40cd6b776723ed9355dee565fa2b28ccd0776
ashbake <abaker@caltech.edu>
2026-09-15 17:15:15 -0700

    add emissivity bkg calc

--- cThroughput.py
+++ cThroughput.py
@@ -28,6 +28,9 @@ import csv,os
 from scipy import interpolate
 from matplotlib.ticker import MultipleLocator
 
+from astropy import units as u
+from astropy.modeling.models import BlackBody
+
 import matplotlib
 font = {'size'   : 14}
 matplotlib.rc('font', **font)
@@ -68,7 +71,9 @@ class CalcThroughput():
         self.data_path = data_path
         
         # load dictionary of transmission data for each subsection then combine
-        self.transmission_dic = self._loadTransmissionData(wave,excel_file,data_path,include_ind)
+        self.includes, self.types, self.values, self.filenames, self.emissivities, self.temperatures, self.elements = self._loadThroughputFile(excel_file)
+
+        self.transmission_dic = self._loadTransmissionData(include_ind)
         
 		#you can then call runThroughputCalc with the set of keys to compute throughput for
         
@@ -105,19 +110,10 @@ class CalcThroughput():
 
         return self.total_throughput
     
-    def _loadTransmissionData(self,wave,excel_file,data_path, include_ind=1):
+    def _loadTransmissionData(self, include_ind=1):
         """ Load transmission data into dictionary
 
-        inputs
-        ------
-        wave: arr
-            wavelength grid
-        excel_file : str
-            path to and name of excel file to load
-        include_ind: int
-            value in the 'Include?' column of the excel file that indicates to include the row in the transmission calculation. 
-            Will always include '1' , never include '0', and will also include include_ind values (for selecting dichroic options)
-            TODO: make include_ind a list to allow for more options in the future
+        Assumes that the excel file has been loaded in __init__ and that the includes, types, values, filenames, and elements arrays are available
 
         outputs
         -------
@@ -125,22 +121,110 @@ class CalcThroughput():
             dictionary with keys as section headers and values as 
             transmission arrays
         """
-        includes, types, values, filenames, elements = self._loadThroughputFile(data_path + excel_file)
-
         transmission = {}
-        for i, include in enumerate(includes):
+        for i, include in enumerate(self.includes):
             # start a dictionary entry for new section
-            if types[i] == 'Note':
-                key = elements[i]
-                transmission[key] = np.ones_like(wave)
+            if self.types[i] == 'Note':
+                key = self.elements[i]
+                transmission[key] = np.ones_like(self.wave)
             # if include (first column) is 1, include it
             if (include == 1) or (include == include_ind):
                 # process input based on entry type and mulitply all together
-                print(key)
-                transmission[key] *= self._setInput(wave,types[i], values[i], 
-                                                   filenames[i],data_path)
+                print(key, self.elements[i])
+                transmission[key] *= self._setInput(self.wave,self.types[i], self.values[i], 
+                                                   self.filenames[i],self.data_path)
 
         return transmission
+
+    def calcBackground(self, keys_to_include, include_ind=1, R=100000, npix=3):
+        """ Compute emissivity
+
+        inputs
+        ------
+        include_ind: int
+            which to include in addition to 1 (for selecting dichroics)
+
+        R: float
+            resolving power of spectrograph (default 100,000)
+        
+        npix: float
+            pixel sampling of spectrograph per resolution element (default 100,000)
+
+        outputs
+        -------
+        path_background - array or float
+            if FEI ATC is in keys_to_include, it will 
+
+        path_background_flux - dictionary
+            has the independent emission of each subsystem
+
+        references:
+        https://caltech.sharepoint.com/:p:/r/sites/coo/hispec/_layouts/15/Doc.aspx?sourcedoc=%7BDB5077D7-912D-4B5F-83DC-2A38FBC6150D%7D&file=Thermal%20Background%20Analytical%20Calculations.pptx&action=edit&mobileredirect=true
+        """ 
+        #keys_to_include = ['TELESCOPE', 'AO', 'FEI COMMON', 'FEI ATC']
+        if 'FEI ATC' in keys_to_include: # if FEI ATC is included, need to assume ATC metrics
+            Aomega = 28.3 * u.radian**2 * u.micron ** 2 # Area times omega assuming f/6 cold snout 1.132 π2 p2 / (4F#2)
+        else:
+            # These are only valid for the spectrograph
+            dlambda  = u.nm * self.wave / R / npix # pixel width in nanometers considering pixel sampling
+            #fwhm = ((self.wave * u.nm / telescope_diameter) * u.radian).to(u.arcsec)
+            #Aomega   = 1.13 * 2 * fwhm **2 * np.pi * (telescope_diameter/2)**2 # this reduces 1.775 * lambda^2
+            Aomega = 1.775 * u.radian**2 * (u.nm * self.wave) ** 2 # area of telescope times solid angle of fiber on sky, approx what couples into the SMF
+
+        path_background_flux = {}                        # for storing snapshots of each subsystem where only considers that subsystem
+        path_background_total = np.zeros_like(self.wave) # for combining full path to include full throughput of path
+
+        for i, include in enumerate(self.includes):
+            # start a dictionary entry for new section
+            if self.types[i] == 'Note':
+                key = self.elements[i]
+                path_background_flux[key] = np.zeros_like(self.wave) # refresh this for each subsystem
+            if key in keys_to_include:
+                print(f'Computer Emissivity - including {key} components')
+                # if include (first column) is 1, include it
+                if (include == 1) or (include==include_ind):
+                    # Load the transmission for this surface
+                    transmission_surface = self._setInput(self.wave,self.types[i], self.values[i], 
+                        self.filenames[i],self.data_path)
+
+                    # Parse Emissivity entry
+                    if (self.emissivities[i] == '1-R') or (self.emissivities[i] == '1-T'):
+                        emissivity_surface = 1 - transmission_surface
+                        flux_out_surface   = emissivity_surface * self._get_bb_radiation(self.temperatures[i] * u.K, Aomega) 
+                    elif not np.isnan(self.emissivities[i]): # should be a number if not 1-R or 1-T or nan
+                        emissivity_surface = self.emissivities[i] * np.ones_like(self.wave)
+                        flux_out_surface   = emissivity_surface * self._get_bb_radiation(self.temperatures[i]*u.K, Aomega) 
+                    else:
+                        flux_out_surface = np.zeros_like(self.wave) # add 0 for line if doesnt have emissivity, but still must consider throughput effect
+
+                    # New flux emissivity of surface gets added to the old emissivity, which is lowered by transmission of this surface
+                    path_background_flux[key] = path_background_flux[key] * transmission_surface + flux_out_surface 
+                    path_background_total     = path_background_total * transmission_surface + flux_out_surface 
+
+        if 'FEI ATC' in keys_to_include:
+            path_background = np.trapezoid(path_background_total, x=u.nm * self.wave).decompose()
+        else:
+            path_background = (path_background_total * dlambda).decompose()
+
+        return path_background, path_background_flux
+
+    def _get_bb_radiation(self, temperature, Aomega):
+        """
+        Compute blackbody radiation flux density for a surface with temperature
+
+        inputs
+        ------
+        temperature - [u.K] 
+            temperature of blackbody
+        
+        Aomega - [u.m^2 * u.arcsec^2]
+            Solid angle times area or etendue of fiber
+        """
+        bb_fxn  = BlackBody(temperature, scale=1.0 * u.erg / (u.micron * u.s * u.cm**2 * u.arcsec**2)) 
+        bb_density      = Aomega * bb_fxn(self.wave * u.nm)
+        bb_flux         = bb_density.to(u.photon/u.s/u.micron, equivalencies=u.spectral_density(u.nm * self.wave)) 
+
+        return bb_flux
 
     def _combineTransmission(self, x,transmission_dic, keys):
         """for key in keys, multiply all transmission entries together."""
@@ -182,13 +266,15 @@ class CalcThroughput():
         # get the number of active surfaces in the excel file
         #num_active_sfcs = df['Include?'].value_counts()[1]
 
-        includes  = df['Include?']
-        types     = df['Type']
+        includes  = df['Include?'] # 0 to exclude, 1 to include, 2-5 selecting which ATC dichroic to assume
+        types     = df['Type']     # coating, constant, or internal transmission
         values    = df['Value']
         filenames = df['Datafile']
-        elements  = df['Element']  #maybe for plotting/grouping
+        emissivities = df['Emissivity']  # populated for coatings that have emissivity data, otherwise NaN
+        temperatures = df['Temperature'] # "
+        elements  = df['Element']  # maybe for plotting/grouping
 
-        return includes, types, values, filenames, elements
+        return includes, types, values, filenames, emissivities, temperatures, elements
 
     def _setInput(self,wave, tt, value, file_name, data_path):
         """
@@ -276,22 +362,21 @@ class CalcThroughput():
         
         Could be rewritten to avoid duplicate loading of excel file..
         """
-        includes, types, values, filenames, elements = self._loadThroughputFile(self.data_path + self.excel_file)
 
         total = np.ones_like(self.wave)
 
         fig, ax = plt.subplots(1, 1, figsize=(8,5))
-        for i, include in enumerate(includes):
+        for i, include in enumerate(self.includes):
             # start a dictionary entry for new section
-            if types[i] == 'Note':
-                key = elements[i]
+            if self.types[i] == 'Note':
+                key = self.elements[i]
             if key==key_name:
                 if include == 1:
-                    t = self._setInput(self.wave,types[i], values[i], filenames[i],self.data_path)
-                    if types[i]=='Coating' or types[i]=='coating':
-                        ax.plot(self.wave,t,label=elements[i])
-                    if types[i]=='Constant' or types[i]=='constant':
-                        ax.plot(self.wave,self.wave*0+t,label=elements[i])
+                    t = self._setInput(self.wave,self.types[i], self.values[i], self.filenames[i],self.data_path)
+                    if self.types[i]=='Coating' or self.types[i]=='coating':
+                        ax.plot(self.wave,t,label=self.elements[i])
+                    if self.types[i]=='Constant' or self.types[i]=='constant':
+                        ax.plot(self.wave,self.wave*0+t,label=self.elements[i])
                     total*=t
 
         ax.fill_between(yJ,y1=0,y2=1,facecolor='blue',alpha=0.1,zorder=-100)
@@ -377,7 +462,7 @@ if __name__=='__main__':
     np.savetxt('inputs/fiber/strehl_howfe_%snm.csv'%wfe, np.vstack((x,strehl)).T,delimiter=',',header='wavelength(nm), strehl')
 
     # example calc throughput usage with atc throughput
-    gbt = CalcThroughput(x, '../HISPEC_allsubs.xlsx',data_path='./inputs/')
+    gbt = CalcThroughput(x, './HISPEC_allsubs.xlsx',data_path='./inputs/')
     atc_keys = ['TELESCOPE', 'AO', 'FEI COMMON', 'FEI ATC']
     label    = 'ATC Throughput'
     throughput = gbt.run(atc_keys)
